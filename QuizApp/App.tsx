d8c3ded3cc607db02c932a3ae823c66de9d16da8
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-03-02 15:14:41 +0400

    Add Picker to choose cattegories

--- QuizApp/App.tsx
+++ QuizApp/App.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
 
-import HomeScreen from './src/screens/HomeScreen';
-import QuizScreen from './src/screens/QuizScreen';
-import ResultScreen from './src/screens/ResultScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import HomeScreen from "./src/screens/HomeScreen";
+import QuizScreen from "./src/screens/QuizScreen";
+import ResultScreen from "./src/screens/ResultScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { QuizProvider } from "../QuizApp/src/context/QuizContext";
 
 const Stack = createNativeStackNavigator();
 
-
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Quiz" component={QuizScreen} />
-        <Stack.Screen name="Result" component={ResultScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QuizProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Quiz" component={QuizScreen} />
+          <Stack.Screen name="Result" component={ResultScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QuizProvider>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
--- QuizApp/src/screens/HomeScreen.tsx
+++ QuizApp/src/screens/HomeScreen.tsx
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Text, StyleSheet, } from "react-native";
+import {Picker} from '@react-native-picker/picker';
 
+import { useQuiz } from "../context/QuizContext";
+import { fetchCategories } from "../utils/api";
 
+import { View } from "react-native";
 
-import { View } from "react-native"
+const HomeScreen: React.FC = ({ }) => {
+  const [categories, setCategories] = useState([]);
+  const { state, dispatch } = useQuiz();
 
+  useEffect(() => {
+    const loadCategories = async () => {
+      const fetchCatefories = await fetchCategories();
+      setCategories(fetchCatefories);
+    };
+    loadCategories();
+  }, []);
 
-const HomeScreen: React.FC = () => {
   return (
-    <View>
-      
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <Text style={styles.title}>Select Category:</Text>
 
-export default HomeScreen
+      <Picker
+        selectedValue={state.settings.category}
+        onValueChange={(itemValue, itemIndex) =>
+          dispatch({
+            type: "SET_SETTINGS",
+            payload: { ...state.settings, category: itemValue },
+          })
+        }
+      >
+        {categories.map((category) => (
+          <Picker.Item
+            key={category.id}
+            label={category.name}
+            value={category.id.toString()}
+          />
+        ))}
+      </Picker>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#eaeaea",
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+},
+});
+
+export default HomeScreen;
